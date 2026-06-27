5d6d8c8b79152837a0de12adc304c0623a994335
JamsBond <34511634+britton477@users.noreply.github.com>
2026-06-27 13:03:35 +0100

    Delete _cleanup.bat

--- _cleanup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "F:\My Drive\CardVault Pro\app"
-git rm -f _deploy-setup.bat _deploy-setup.ps1 2>nul
-git add -A
-git commit -m "chore: remove deploy setup scripts"
-git push origin main
-del /f /q "F:\My Drive\CardVault Pro\app\_cleanup.bat"
